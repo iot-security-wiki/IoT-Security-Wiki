94a3d4bfc50e662a046e857bce62b09461f447d3
BG7YWL <adm7n@Outlook.com>
2020-10-16 15:15:09 +0800

    update commit

---
+++ node_modules/gitbook-plugin-baidu-v3/README.md
@@ -0,0 +1,2 @@
+# gitbook-plugin-baidu-v3
+gitbook-v3百度统计插件
---
+++ node_modules/gitbook-plugin-baidu-v3/index.js
@@ -0,0 +1,9 @@
+module.exports = {
+    // Extend website resources and html
+    book: {
+        assets: "./book",
+        js: [
+            "baidu-tongji.js"
+        ]
+    }
+};
